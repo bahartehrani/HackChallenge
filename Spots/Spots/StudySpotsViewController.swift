2c2a5249eca5d5a4d11693240e20016e13cf6ad4
bahartehrani <35770469+bahartehrani@users.noreply.github.com>
2019-12-03 20:00:45 -0500

    Implemented table view for study spots

--- Spots/Spots/StudySpotsViewController.swift
+++ Spots/Spots/StudySpotsViewController.swift
@@ -14,16 +14,33 @@ import SnapKit
 
 class StudySpotsViewController: UIViewController {
     
+    // header view
     var collectionView : UICollectionView!
+    
     let padding : CGFloat = 8
     let categoriesCellReuseIdentifier = "categoriesCellReuseIdentifier"
+    
+    // categories
     var categories : [String]!
+    // selected filters
     var selectedFilters : [String] = []
+    
+    // title
     var viewTitle : UILabel!
+    
+    // table view for study spots
+    var tableView : UITableView!
+    let reuseIdentifier = "spotsCellReuse"
+    
+    var spots : [Spot] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        let spot1 = Spot(name: "Carrot1")
+        let spot2 = Spot(name: "Carrot2")
+        let spot3 = Spot(name: "Carrot3")
+        spots = [spot1, spot2, spot3]
         
         view.backgroundColor = UIColor(red: 13/255, green: 12/255, blue: 23/255, alpha: 1.0)
         
@@ -31,7 +48,7 @@ class StudySpotsViewController: UIViewController {
         viewTitle.textAlignment = .center
         viewTitle.textColor = UIColor(red: 209/255, green: 211/255, blue: 217/255, alpha: 1.0)
         viewTitle.text = "Study Spots"
-        viewTitle.font = UIFont.boldSystemFont(ofSize: 40.0)
+        viewTitle.font = UIFont.boldSystemFont(ofSize: 32.0)
         view.addSubview(viewTitle)
         
         
@@ -49,9 +66,16 @@ class StudySpotsViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.allowsMultipleSelection = true
-        
         view.addSubview(collectionView)
         
+        tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .white
+        tableView.register(SpotsTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        view.addSubview(tableView)
+        
+
         categories = ["Open", "Closed", "Quiet", "North", "West", "Central"]
         
         setupConstraints()
@@ -65,7 +89,7 @@ class StudySpotsViewController: UIViewController {
     
     func setupConstraints() {
         collectionView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(padding * 15)
+            make.top.equalToSuperview().offset(padding * 13)
             make.leading.equalToSuperview().offset(padding)
             make.trailing.equalToSuperview().offset(-padding)
             make.height.equalTo(6 * padding)
@@ -73,10 +97,21 @@ class StudySpotsViewController: UIViewController {
         }
         
         viewTitle.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(padding * 4)
+            make.top.equalToSuperview().offset(padding * 5)
             make.centerX.equalTo(view.center.x)
             make.height.equalTo(50)
         }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(collectionView).offset(padding * 6)
+            make.height.equalTo(100)
+            make.centerX.equalTo(view.center.x)
+            make.centerY.equalTo(view.center.y)
+            make.width.equalTo(300)
+        }
+        
+        
+        
     }
 
 }
@@ -120,4 +155,25 @@ extension StudySpotsViewController : UICollectionViewDelegate {
         selectedFilters.append(selectedFilter)
     
     }
+}
+
+extension StudySpotsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SpotsTableViewCell
+        cell.configure(for: spots[indexPath.row])
+        cell.selectionStyle = .none
+        return cell
+    }
+     
+}
+
+extension StudySpotsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 15
+    }
+    
 }
