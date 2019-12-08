0fa8a55182731cce82cf12608630c4e4e0aeba2e
Angela <angelachenchen19@gmail.com>
2019-12-08 16:26:59 -0500

    DetailedTableView
    Making the detailed table view

--- Spots/Spots/DetailedSpotViewController.swift
+++ Spots/Spots/DetailedSpotViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class DetailedSpotViewController: UIViewController {
+    
+    var tableView: UITableView!
+    let reuseIdentifier = "detailedCellReuse"
+    let cellHeight: CGFloat = 250
+    var rows : [String] = ["hours", "popularTimes", "resources"]
 
     var viewTitle : UILabel!
     var zoomedPic : UIImageView!
     
-    var popTimes : UILabel!
-    var popTimesImage : UIImageView!
-    
-    var resName : UILabel!
-    var resources : UILabel!
+//    var hours : UILabel!
+//
+//    var popTimes : UILabel!
+//    var popTimesImage : UIImageView!
+//
+//    var resName : UILabel!
+//    var resources : UILabel!
     
     let padding : CGFloat = 8
     
@@ -33,6 +40,38 @@ class DetailedSpotViewController: UIViewController {
         viewTitle.font = UIFont.boldSystemFont(ofSize: 32.0)
         view.addSubview(viewTitle)
         
+        zoomedPic = UIImageView(image: UIImage(named: "Testing"))
+        zoomedPic.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(zoomedPic)
+        
+        tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = UIColor(red: 13/255, green: 12/255, blue: 23/255, alpha: 1.0)
+        tableView.register(DetailedViewTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        view.addSubview(tableView)
+        
+//        hours = UILabel()
+//        hours.textAlignment = .left
+//        hours.textColor = UIColor(red: 209/255, green: 211/255, blue: 217/255, alpha: 1.0)
+//        hours.text = "Hours"
+//        viewTitle.font = UIFont.boldSystemFont(ofSize: 15.0)
+//        view.addSubview(hours)
+//
+//        popTimes = UILabel()
+//        popTimes.textAlignment = .left
+//        popTimes.textColor = UIColor(red: 209/255, green: 211/255, blue: 217/255, alpha: 1.0)
+//        popTimes.text = "Popular Times"
+//        viewTitle.font = UIFont.boldSystemFont(ofSize: 15.0)
+//        view.addSubview(popTimes)
+//
+//        resources = UILabel()
+//        resources.textAlignment = .left
+//        resources.textColor = UIColor(red: 209/255, green: 211/255, blue: 217/255, alpha: 1.0)
+//        resources.text = "Hours"
+//        viewTitle.font = UIFont.boldSystemFont(ofSize: 15.0)
+//        view.addSubview(resources)
+        
         setupConstraints()
         
     }
@@ -44,11 +83,62 @@ class DetailedSpotViewController: UIViewController {
     func setupConstraints() {
         
         viewTitle.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(padding * 20)
+            make.top.equalToSuperview().offset(padding * 28.5)
             make.centerX.equalTo(view.center.x)
             make.height.equalTo(50)
         }
+        
+        zoomedPic.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.height.equalTo(220)
+            make.width.equalToSuperview()
+        }
+        
+//        hours.snp.makeConstraints { make in
+//            make.top.equalToSuperview().offset(padding * 35)
+//            make.leading.equalToSuperview().offset(padding)
+//        }
+//
+//        popTimes.snp.makeConstraints { make in
+//            make.top.equalToSuperview().offset(padding * 70)
+//            make.leading.equalToSuperview().offset(padding)
+//        }
+//
+//        resources.snp.makeConstraints { make in
+//            make.top.equalToSuperview().offset(padding * 105)
+//            make.leading.equalToSuperview().offset(padding)
+//        }
+        
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(padding * 35)
+            make.bottom.equalTo(view.snp.bottom)
+            make.leading.equalToSuperview().offset(padding)
+            make.trailing.equalToSuperview().offset(-padding)
+
+        }
     }
+    
 }
 
+extension DetailedSpotViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+        tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! DetailedViewTableViewCell
+        //cell.configure(for: rows[indexPath.row])
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
+}
 
+extension DetailedSpotViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeight
+    }
+
+}
